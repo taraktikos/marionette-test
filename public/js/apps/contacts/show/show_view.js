2bad1ebc88f5bd49b7b3ce8f5c9856d3b2935b2d
tsuhovenko <tsuhovenko@cogniance.com>
2014-12-04 16:13:34 +0200

    not found page

--- public/js/apps/contacts/show/show_view.js
+++ public/js/apps/contacts/show/show_view.js
@@ -1,5 +1,9 @@
 ContactManager.module("ContactsApp.Show", function(Show, ContactManager, Backbone, Marionette, $, _) {
 
+	Show.MissingContact = Marionette.ItemView.extend({
+		template: "#missing-contact-view"
+	});
+
 	Show.Contact = Backbone.Marionette.ItemView.extend({
     	template: "#contact-view"
     });
